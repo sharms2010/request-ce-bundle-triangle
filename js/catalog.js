4df6069c7f74d1d2f5a7ffbdc7ebb723f3531873
ChadRehmKineticData <chad.rehm@kineticdata.com>
2015-12-15 11:50:00 -0600

    auto update to the alert bundle

--- js/catalog.js
+++ js/catalog.js
@@ -1,5 +1,13 @@
  // jQuery GET call used to get updates from the Alerts and Outages bundle
+function getAlert(){
 $.getJSON("alerts/for-display", function(data){
-    $('.alert-outages-body').append($('<p>', { text : data.date}));
+    $('.alert-outages-body').empty().append($('<p>', { text : data.date}));
     $('.alert-outages-body').append($('<div>', { text : data.details}));
-});
+}).always(function() {
+    setAlertInterval();;
+  });
+};
+function setAlertInterval(){
+    setTimeout(getAlert, 5000);
+}
+getAlert();
